0fed6b97e4ce4f66caab0f9666f0a68be0edf1b4
Jim Tyhurst <jim@tyhurst.com>
2021-01-06 16:49:17 -0800

    Fixes function syntax. These functions work as expected.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,46 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+"use strict";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from FriendlyEats!");
+admin.initializeApp();
+
+const APP_NAME = "FriendlyEats!";
+const VERSION = "1.0.2"
+
+
+export const helloFriend = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!", {structuredData: true});
+    response.status(200).send({
+        message: "Hello from " + APP_NAME,
+        function: "helloWorld",
+        version: VERSION,
+    });
+});
+
+
+export const getHighValues = functions.https.onRequest((request, response) => {
+    const FUNCTION_NAME = "getHighValues";
+    admin.firestore()
+        .collection('restaurants')
+        .where("valueIndex", ">", 1)
+        .get()
+        .then(function(snapshot) {
+            response.status(200)
+                .send({
+                    function: FUNCTION_NAME,
+                    version: VERSION,
+                    data: snapshot.docs,
+                });
+        })
+        .catch(function(error) {
+            response.status(500)
+                .send({
+                    function: FUNCTION_NAME,
+                    version: VERSION,
+                    error: {
+                        message: "Error getting high value restaurants.",
+                        data: error,
+                    },
+                });
+        });
 });
